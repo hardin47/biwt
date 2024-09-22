be7e9ea21c8a9373f68f5f338440fa6fe5a78fc7
hardin47 <jo.hardin@pomona.edu>
2024-09-22 15:02:43 -0700

    fix tests

--- tests/testthat/test-biwt.R
+++ tests/testthat/test-biwt.R
@@ -4,8 +4,11 @@ test_that("function runs", {
                                           Sigma=matrix(c(1,.75,-.75,.75,1,-.75,-.75,-.75,1),
                                                        ncol=3))
 
-               r<-0.2 # breakdown
-
-               biwt_cor(samp.data[,1:2], r=.2)$biwt_cor}, 0.6605525,
+               biwt_cor(samp.data[,1:2], r=0.2)$biwt_cor},
+               {set.seed(4747)
+                 samp.data <- MASS::mvrnorm(30, mu=c(0,0,0),
+                                            Sigma=matrix(c(1,.75,-.75,.75,1,-.75,-.75,-.75,1),
+                                                         ncol=3))
+               biwt.cor(t(samp.data[,1:2]), r = 0.2, output = "vector")},
                tolerance = 0.01)
 })
